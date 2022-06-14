1ddafcd2ed13bda44bc6bb5f370213ae3da28421
Andallfor <leozwang2005@gmail.com>
2022-06-14 00:21:01 -0400

    fix for image shearing

--- Assets/Code/data classes/geographic.cs
+++ Assets/Code/data classes/geographic.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public struct geographic
--- Assets/Code/terrain/dtedImageCombiner.cs
+++ Assets/Code/terrain/dtedImageCombiner.cs
@@ -14,16 +14,13 @@ To generate a texture for each dted file, we are using sentinel images. However,
 Because of this, we need to combine multiple sentinel textures to create one full texture. 
 Due to the way the sentinel textures are formatted, one area can have multiple textures with different areas filled in and different areas left blank. We take these textures that cover the same area and effectively layer them on top of each other to mask the parts left blank.
 However, the sentinel files are not perfectly accurate. The texture provided is a square, implying that either sides should be parallel to each other. But the area covered by the texture (including the blank areas) is not a square. There is a few percent difference between opposite sides, so there will be minor distortation in the resultant texture.
-However, because each texture covers the exact same area, there should not be any noticable image splicing- its just that when overlayed on the dted mesh, the texture will be every so slightly off.
-TODO: This may be something we want to account for later, but for the moment the percent diff is small enough that it should be fine to ignore.
-TODO: Actually check if this is still true. We switched from manually calculating bounds to pulling it from offical sources, so the inaccuracies may not longer be true.
+So, when overlayed on the dted mesh, the texture will be every so slightly off.
 */
-
 
 public static class dtedImageCombiner
 {
     private static List<sentinelFile> sentFiles = new List<sentinelFile>();
-    private const double resolution = 8192; // This value must be less then sentinelFile.imageSize
+    private const double resolution = 10980; // This value must be <= then sentinelFile.imageSize
     public static Texture2D generateImage(geographic boundMin, geographic boundMax, string imageFolder) {
         DirectoryInfo folder = new DirectoryInfo(imageFolder);
         foreach (DirectoryInfo tile in folder.EnumerateDirectories()) {
@@ -43,33 +40,30 @@ public static class dtedImageCombiner
                 Color32 c = new Color32(0, 0, 0, 0);
                 geographic point = boundMin + new geographic(yIncrement * y, xIncrement * x);
 
+                bool pixelExists = false;
                 foreach (sentinelArea sa in areas) {
-                    // check if the point were requesting is out of bounds for the current area
-                    // if so, continue and see if the next area contains said point
-                    if (point.lon > sa.corners[1].lon || point.lat > sa.corners[1].lat || point.lon < sa.corners[3].lon || point.lat < sa.corners[3].lat) continue;
+                    double xIndex = sentinelFile.imageSize * ((point.lon - sa.lines[3].xIntersection(point.lat)) / (sa.lines[1].xIntersection(point.lat) - sa.lines[3].xIntersection(point.lat)));
+                    double yIndex = sentinelFile.imageSize - sentinelFile.imageSize * ((point.lat - sa.lines[2].yIntersection(point.lon)) / (sa.lines[0].yIntersection(point.lon) - sa.lines[2].yIntersection(point.lon)));
 
-                    double xIndex = sentinelFile.imageSize * ((point.lon - sa.corners[3].lon) / (sa.corners[2].lon - sa.corners[3].lon));
-                    double yIndex = sentinelFile.imageSize * ((point.lat - sa.corners[3].lat) / (sa.corners[0].lat - sa.corners[3].lat));
-
-                    int xi = (int) Math.Min(sentinelFile.imageSize - 1, Math.Max(0, xIndex));
-                    int yi = (int) Math.Min(sentinelFile.imageSize - 1, Math.Max(0, sentinelFile.imageSize - yIndex)); // idk why its max - yIndex, it just works
+                    if (xIndex < 0 || xIndex > sentinelFile.imageSize - 1 || yIndex < 0 || yIndex > sentinelFile.imageSize - 1) continue;
 
                     foreach (sentinelFile sf in sa.files) {
                         // check to see if the image has the desired point
-                        Rgb24 pixel = sf.texture[xi, yi];
+                        Rgb24 pixel = sf.texture[(int) Math.Round(xIndex), (int) Math.Round(yIndex)];
 
                         // if the pixel is black (0, 0, 0) that means that the file does not have the desired pixel
                         if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0) continue;
                         else {
                             c = new Color32(pixel.R, pixel.G, pixel.B, 255);
                             break;
-                        }
-                        
+                        }                        
                     }
 
                     // if the alpha changes that means weve found a valid pixel, so no need to process more
                     if (c.a != 0) break;
                 }
+
+                if (pixelExists && c.a == 0) c = new Color32(0, 255, 255, 255);
 
                 texture.SetPixel((int) x, (int) y, c);
             }    
@@ -128,6 +122,10 @@ public class sentinelArea {
     public string tile;
     /// <summary> NW, NE, SE, SW </summary>
     public List<geographic> corners = new List<geographic>();
+    /// <summary> N, E, S, W </summary>
+    public List<double> sides = new List<double>();
+    /// <summary> N, E, S, W </summary>
+    public List<line> lines = new List<line>();
     public geographic center;
     public List<sentinelFile> files;
 
@@ -149,6 +147,18 @@ public class sentinelArea {
         };
 
         foreach (string[] corner in bounds) corners.Add(new geographic(double.Parse(corner[0]), double.Parse(corner[1])));
+
+        sides.Add(Math.Max(corners[0].lat, corners[1].lat)); // N
+        sides.Add(Math.Max(corners[1].lon, corners[2].lon)); // E
+        sides.Add(Math.Min(corners[2].lat, corners[3].lat)); // S
+        sides.Add(Math.Min(corners[3].lon, corners[1].lon)); // W
+
+        lines = new List<line>() {
+            new line(new List<geographic>() {corners[0], corners[1]}), // N
+            new line(new List<geographic>() {corners[1], corners[2]}), // E
+            new line(new List<geographic>() {corners[2], corners[3]}), // S
+            new line(new List<geographic>() {corners[3], corners[0]})  // W
+        };
     }
 
     public static List<sentinelArea> sortFiles(List<sentinelFile> files) {
@@ -167,7 +177,6 @@ public class sentinelArea {
         return output;
     }
 }
-
 
 /// <summary> Represents a singular sentinel file (folder?). </summary>
 public class sentinelFile {
@@ -204,4 +213,25 @@ public class sentinelFile {
         DirectoryInfo di = new DirectoryInfo(Path.Combine(folder, "GRANULE")).GetDirectories()[0].GetDirectories().First(x => x.Name == "IMG_DATA");
         texture = Image<Rgb24>.Load(di.GetFiles().First(x => x.Name.Contains(".png")).FullName) as Image<Rgb24>;
     }
+}
+
+// TODO: move this somewhere more appropiate
+public struct line {
+    public double m, b;
+    public List<geographic> points;
+    public line(double m, double b, List<geographic> points) {
+        this.m = m;
+        this.b = b;
+        this.points = points;
+    }
+
+    public line(List<geographic> points) {
+        this.m = (points[1].lat - points[0].lat) / (points[1].lon - points[0].lon);
+        this.b = points[0].lat - this.m * points[0].lon;
+
+        this.points = points;
+    }
+
+    public double xIntersection(double y) => (y - b) / m;
+    public double yIntersection(double x) => x * m + b;
 }
