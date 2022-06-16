5c511c522a59f5d73e3cf861a865fe0d65f7f1d4
TheLegendaryGanjoo <70727255+TheLegendaryGanjoo@users.noreply.github.com>
2022-06-16 11:56:19 -0400

    Added vector functions

--- Assets/Code/data classes/visibilty.cs
+++ Assets/Code/data classes/visibilty.cs
@@ -188,4 +188,150 @@ public static class visibility
     raycastInfo raycastInfo = new raycastInfo(hit, hitPlanets, hitSats, hitFacs);
     return raycastInfo;
   }
+
+//vector functions, the second number is the vector
+public static raycastInfo raycastvector(position p1, position vector, raycastParameters flags, int defaultRadius)
+{
+  // 100 indicates planet search
+  // 10 indicates a satellite search
+  // 1 indicates a facility search
+
+  List<planet> hitPlanets = new List<planet>();
+  List<facility> hitFacs = new List<facility>();
+  List<satellite> hitSats = new List<satellite>();
+  bool hit = false;
+
+  position p2 = new position(
+  double.MaxValue/4 + vector.x,
+   double.MaxValue/4 + vector.y,
+    double.MaxValue/4 + vector.z);
+
+  if ((flags & raycastParameters.planet) == raycastParameters.planet)
+  {
+    foreach(planet p in master.allPlanets)
+    {
+      if (distance(p1, p.pos) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, p.pos);
+        if (distanceFromObj <= p.radius)
+        {
+          hitPlanets.Add(p);
+          hit = true;
+          break;
+        }
+      }
+    }
+  }
+
+  if ((flags & raycastParameters.satellite) == raycastParameters.satellite)
+  {
+    foreach(satellite sat in master.allSatellites)
+    {
+      if (distance(p1, sat.pos) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, sat.pos);
+        if (distanceFromObj <= defaultRadius)
+        {
+          hitSats.Add(sat);
+          hit = true;
+          break;
+        }
+      }
+    }
+  }
+
+  if ((flags & raycastParameters.facility) == raycastParameters.facility)
+  {
+    foreach(facility f in master.allFacilites)
+    {
+      //alt is set to 0 now but eventually when altitudes are implemented in geographic positions they will be added here too
+      double alt = 0;
+      position fac = f.facParent.geoOnPlanet(f.geo, alt);
+      if (distance(p1, fac) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, fac);
+        if (distanceFromObj <= defaultRadius)
+        {
+          hitFacs.Add(f);
+          hit = true;
+          break;
+        }
+      }
+    }
+  }
+
+  raycastInfo raycastInfo = new raycastInfo(hit, hitPlanets, hitSats, hitFacs);
+  return raycastInfo;
+}
+
+public static raycastInfo raycastvectorHit(position p1, position vector, raycastParameters flags, int defaultRadius)
+{
+  // 100 indicates planet search
+  // 10 indicates a satellite search
+  // 1 indicates a facility search
+
+  List<planet> hitPlanets = new List<planet>();
+  List<facility> hitFacs = new List<facility>();
+  List<satellite> hitSats = new List<satellite>();
+  bool hit = false;
+
+  position p2 = new position(
+  double.MaxValue/4 + vector.x,
+   double.MaxValue/4 + vector.y,
+    double.MaxValue/4 + vector.z);
+
+  if ((flags & raycastParameters.planet) == raycastParameters.planet)
+  {
+    foreach(planet p in master.allPlanets)
+    {
+      if (distance(p1, p.pos) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, p.pos);
+        if (distanceFromObj <= p.radius)
+        {
+          hit = true;
+          hitPlanets.Add(p);
+        }
+      }
+    }
+  }
+
+  if ((flags & raycastParameters.satellite) == raycastParameters.satellite)
+  {
+    foreach(satellite sat in master.allSatellites)
+    {
+      if (distance(p1, sat.pos) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, sat.pos);
+        if (distanceFromObj <= defaultRadius)
+        {
+          hit = true;
+          hitSats.Add(sat);
+        }
+      }
+    }
+  }
+
+  if ((flags & raycastParameters.facility) == raycastParameters.facility)
+  {
+    foreach(facility f in master.allFacilites)
+    {
+      //alt is set to 0 now but eventually when altitudes are implemented in geographic positions they will be added here too
+      double alt = 0;
+      position fac = f.facParent.geoOnPlanet(f.geo, alt);
+      if (distance(p1, fac) > distance(p1,p2))
+      {
+        double distanceFromObj = raycastMath(p1, p2, fac);
+        if (distanceFromObj <= defaultRadius)
+        {
+          hit = true;
+          hitFacs.Add(f);
+        }
+      }
+    }
+  }
+
+  raycastInfo raycastInfo = new raycastInfo(hit, hitPlanets, hitSats, hitFacs);
+  return raycastInfo;
+  }
 }
