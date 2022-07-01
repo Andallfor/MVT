7a3eafbd49e21eb63d7da398caf33983dda56dbd
Andallfor <leozwang2005@gmail.com>
2022-07-01 18:08:35 -0400

    Pip support for windows
    See test.py for instructions on how to use

--- Assets/StreamingAssets/pythonScripts/test.py
+++ Assets/StreamingAssets/pythonScripts/test.py
@@ -1,5 +1,6 @@
 import time
 import sys
+import numpy as np
 
 # ALL PRINT STATMENTS MUST HAVE flush=True
 print(f"received args {sys.argv}", flush=True)
@@ -8,6 +9,16 @@ for i in range(10):
     print(i, flush=True)
     time.sleep(0.5)
 
+arr = np.array([1, 2, 3, 4])
+arr = arr * -1
+print(arr, flush=True)
+
 print(i/0, flush=True)
 
 # the last thing outputed by the program is always 'Null'
+
+'''
+HOW TO USE PIP (windows):
+
+<path to unity project>/Assets/StreamingAssets/pythonWindows/Scripts/pip.exe install <name of module>
+'''
