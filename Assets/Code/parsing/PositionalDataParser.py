bae0a433802cc6c3d0cf207f90242401a2c8f4d8
adimachinelearning <adimachinelearning@gmail.com>
2022-07-08 18:18:18 -0400

    Made SQL database for positional data

--- Assets/Code/parsing/PositionalDataParser.py
+++ Assets/Code/parsing/PositionalDataParser.py
@@ -1,1 +1,25 @@
-print("lol")
+import csv
+import sqlite3 as sql
+
+conn = sql.connect("parsing/PositionalData.db")
+cursor = conn.cursor()
+
+command = '''
+CREATE TABLE IF NOT EXISTS PositionalData (
+    Date DOUBLE,
+    X DOUBLE,
+    Y DOUBLE,
+    Z DOUBLE
+)
+'''
+cursor.execute(command)
+conn.commit()
+
+with open('parsing/TESS.csv', 'r') as file:
+
+    for line in file:
+        cursor.execute("INSERT INTO PositionalData VALUES (?,?,?,?)", line.split(","))
+
+conn.commit()
+conn.close()
+        
