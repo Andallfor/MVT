cdcc7fe55fc4f0dd4c83dea99d833120ddd2b832
adimachinelearning <adimachinelearning@gmail.com>
2022-07-12 12:51:37 -0400

    Set up struct

--- Assets/Code/scheduler/ScheduleStructGenerator.cs
+++ Assets/Code/scheduler/ScheduleStructGenerator.cs
@@ -7,12 +7,36 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System;
 using System.Data;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 public static class ScheduleStructGenerator
 {
 
-    public static void testing()
+    public static void genStruct()
     {
-        Debug.Log("Hello world");
+        string filePath = "Assets/Resources/SchedulingJSONS/LunarWindows-ArtemisIII_06_30_22.json";
+        JObject json = JObject.Parse(File.ReadAllText(filePath)); 
+        Scenario scenario = new Scenario();
+        
     }
+}
+
+
+public struct Scenario
+{
+    public string epochTime;
+    public string fileGenDate;
+    public List<Window> windows;
+}
+
+public struct Window
+{
+    public string freqency;
+    public string source;
+    public string destination;
+    public double rate;
+    public double start;
+    public double stop;
+    public double duration;
 }
