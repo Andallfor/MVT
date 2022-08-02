6200f0a902f1eca937b93113905c5a0157e45c2f
424aman <70731645+424aman@users.noreply.github.com>
2022-08-02 03:00:15 -0400

    tidbit start commit

---
+++ Assets/Code/UI/tidbits.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tidbits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string Earth, Moon, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune;
+    private string LCNuno, LCNduo, LCNtrio, Moonlightuno, Moonlightduo, Cubesatuno, Cubesatduo;
+    private string HLSNRHO, HLSDOCKED, HLSDisposal, OriontransitO, OrionDOCKED, OrionNRHO, OriontransitR;
+
+    void Start()
+    {
+        Earth="";
+
+        Moon="The moon is a natural satelite of Earth, about 239,000 miles from Earth on avg. Famous for controlling tides, the moon was formed after a collision between Earth and a planet known as Theia.  Humans have explored the moon via the Apollo Era Missions, however this is soon to change with the planned Artemis Missions.";
+
+        Mercury=" ";
+
+        Venus=" ";
+
+        Mars=" ";
+
+        Jupiter=" ";
+
+        Saturn=" ";
+
+        Uranus=" ";
+
+        Neptune=" ";
+
+        LCNuno="";
+
+        LCNduo=" ";
+
+        LCNtrio=" ";
+
+        Moonlightuno=" ";
+
+        Moonlightduo=" ";
+
+        Cubesatuno=" ";
+
+        Cubesatduo=" ";
+
+        HLSNRHO=" ";
+
+        HLSDOCKED=" ";
+
+        HLSDisposal=" ";
+
+        OriontransitO=" ";
+
+        OrionDOCKED=" ";
+
+        OrionNRHO=" ";
+
+        OriontransitR=" ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
