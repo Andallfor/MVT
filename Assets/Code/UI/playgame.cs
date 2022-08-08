fafd7c51a813547ae4c7c8b2e9ef77a40b17a966
424aman <70731645+424aman@users.noreply.github.com>
2022-08-08 13:38:07 -0400

    start screen update

--- Assets/Code/UI/playgame.cs
+++ Assets/Code/UI/playgame.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playgame : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Canvas info;
+    void Start(){
+        info.enabled=false;
+    }
     public void Playgame(){
         SceneManager.LoadScene(1);
+    }
+    public void showinfo(){
+        info.enabled=true;
+    }
+    public void hideinfo(){
+        info.enabled=false;
     }
 }
