20d40ef171f672ef6e0e644ab1979bfa20757ee9
424aman <70731645+424aman@users.noreply.github.com>
2022-08-08 23:28:32 -0400

    start page update

--- Assets/Code/UI/playgame.cs
+++ Assets/Code/UI/playgame.cs
@@ -7,17 +7,54 @@ using UnityEngine.UI;
 public class playgame : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Canvas info;
+    public Canvas info, controls, scenario;
+    //public Button Info, Control, Scenario;
     void Start(){
         info.enabled=false;
+        controls.enabled=false;
+        scenario.enabled=false;
+        showbuttons();
+        hidebuttons();
+    }
+    public void test(){
+        Debug.Log("clicked");
+    }
+    public void showbuttons(){
+        //Info.enabled=true;
+        //Control.enabled=true;
+        //Scenario.enabled=true;
+    }
+    public void hidebuttons(){
+        //Info.enabled=false;
+        //Control.enabled=false;
+        //Scenario.enabled=false;
     }
     public void Playgame(){
         SceneManager.LoadScene(1);
     }
     public void showinfo(){
         info.enabled=true;
+        //hidebuttons();
+        Debug.Log("clicked");
     }
     public void hideinfo(){
         info.enabled=false;
+        showbuttons();
+    }
+    public void showcontrols(){
+        controls.enabled=true;
+        hidebuttons();
+    }
+    public void hidecontrols(){
+        controls.enabled=false;
+        showbuttons();
+    }
+    public void showscenario(){
+        scenario.enabled=true;
+        hidebuttons();
+    }
+    public void hidescenario(){
+        scenario.enabled=false;
+        showbuttons();
     }
 }
