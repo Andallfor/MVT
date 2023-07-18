705597111703ec9f9119201a308e6178e6a6da96
Andallfor <leozwang2005@gmail.com>
2023-07-18 10:15:36 -0400

    fix minor mistakes

--- Assets/Code/other/accessCallGenerator.cs
+++ Assets/Code/other/accessCallGenerator.cs
@@ -33,22 +33,17 @@ public class accessCallGeneratorWGS {
         meshFile = new universalTerrainJp2File(path, false);
 
         double alt = meshFile.getHeight(pos);
-        //double alt = 0;
         altitude = alt;
-        //alt = 0.55;
-        worldPositionWithHeight = pos.toCartesianWGS(alt);
+        worldPositionWithHeight = pos.toCartesianWGS(alt + 0.01);
         unityPositionWithHeight = (Vector3) (worldPositionWithHeight.swapAxis() / master.scale);
 
         master.currentPosition = earth.representation.gameObject.transform.rotation * (Vector3) worldPositionWithHeight.swapAxis();
 
         // draw terrain
         meshDist = meshFile.load(start, end, 0, res, -worldPositionWithHeight);
-        //meshDist = meshFile.load(start, end, 0, res, default(position));
         meshDist.drawAll(GameObject.FindGameObjectWithTag("fakeMeshParent").transform);
-        //meshDist.drawAll(earth.representation.gameObject.transform);
-        foreach (universalTerrainMesh mesh in meshDist.allMeshes) {
+        foreach (universalTerrainMesh mesh in meshDist.allMeshesOrdered) {
             mesh.addCollider();
-            mesh.go.transform.position = -(Vector3) ((master.currentPosition - earth.representation.gameObject.transform.rotation * (Vector3) worldPositionNoHeight.swapAxis()) / master.scale);
             mesh.go.transform.rotation = earth.representation.gameObject.transform.rotation;
             mesh.hide();
         }
@@ -372,20 +367,15 @@ public class accessCallGeneratorWGS {
         // (0,0,0) because we center (via master.currentPosition) on the correct starting position
         // raycast instead of linecast to prevent physics from checking too much (we really only need to check whats nearby)
         bool result = Physics.Raycast(Vector3.zero, dst, 100, (1 << 6) | (1 << 7)); // terrain and planets only
-        Debug.DrawLine(Vector3.zero, dst, result ? Color.red : Color.green, 10000000);
+        //Debug.DrawLine(Vector3.zero, dst, result ? Color.red : Color.green, 10000000);
         return result;
     }
 
     private void updateMeshes() {
-        master.currentPosition = earth.representation.gameObject.transform.rotation * (Vector3) worldPositionWithHeight.swapAxis();
-
         Quaternion earthRot = earth.representation.gameObject.transform.rotation;
-        //master.currentPosition = earthRot * (Vector3) worldPositionNoHeight; // TODO
-        //master.currentPosition = ((quaternionDouble) earthRot).mult(worldPositionNoHeight.swapAxis());
-        foreach (universalTerrainMesh m in meshDist.allMeshesOrdered) {
-            //m.go.transform.position = -(Vector3) ((master.currentPosition - earth.representation.gameObject.transform.rotation * (Vector3) worldPositionNoHeight.swapAxis()) / master.scale);
-            m.go.transform.rotation = earthRot;
-        }
+        master.currentPosition = earthRot * (Vector3) worldPositionWithHeight.swapAxis();
+        
+        foreach (universalTerrainMesh m in meshDist.allMeshesOrdered) m.go.transform.rotation = earthRot;
         foreach (universalTerrainMesh m in meshWGS.allMeshesOrdered) {
             m.go.transform.position = -(Vector3) (master.currentPosition / master.scale);
             m.go.transform.rotation = earthRot;
