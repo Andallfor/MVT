3505bcbed25e6ca34d0ad059969e1848e4eddad8
Andallfor <leozwang2005@gmail.com>
2023-07-31 23:44:30 -0400

    smooth controls

--- Assets/Code/UI/modes/defaultMode.cs
+++ Assets/Code/UI/modes/defaultMode.cs
@@ -10,10 +10,59 @@ public sealed class defaultMode : IMode {
     protected override bool enable() => true;
     protected override bool disable() => true;
 
-    protected override void _initialize() {}
+    protected override void _initialize() {
+        rotationInterp = new v3Interp(1);
+    }
+
+    private Vector3 rotation;
+    private v3Interp rotationInterp;
+
+    private double scaleChange;
 
     public override void update() {
+        // controls
+        // scroll- change zoom level- this should scale based on how zoomed in/out we are
+        // left click + drag- rotate camera about reference frame
+        // right click + drag- rotate camera
+        // middle click + drag- pan camera- this should scale as well
+        // wasd doesnt do anything
+
+        if (Input.GetMouseButton(0) && !Input.GetMouseButtonUp(0)) {
+            // we want the drag to match how much it looks like it should drag on screen
+            Vector3 difference = Input.mousePosition - playerControls.lastMousePos;
+
+            Vector2 adjustedDifference = new Vector2(-difference.y / Screen.height, difference.x / Screen.width);
+            adjustedDifference *= 75f;
+
+            rotation.x = adjustedDifference.x;
+            rotation.y = adjustedDifference.y;
+            rotation.z = 0;
+
+            rotationInterp.stop();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            rotationInterp.stop();
+            rotationInterp.mark(rotation, Vector3.zero);
+        }
+
+        if (Input.mouseScrollDelta.y != 0) {
+            scaleChange += -(0.1 * master.scale) * Mathf.Sign(Input.mouseScrollDelta.y);
+        }
+
+        general.camera.transform.RotateAround(Vector3.zero, general.camera.transform.right, rotation.x);
+        general.camera.transform.RotateAround(Vector3.zero, general.camera.transform.up, rotation.y);
+
+        general.camera.transform.rotation *= Quaternion.AngleAxis(rotation.z, Vector3.forward);
+
+        if (rotationInterp.isRunning) rotation = rotationInterp.get();
         
+        if (scaleChange != 0) {
+            double c = scaleChange * UnityEngine.Time.deltaTime * 10.0;
+            if (Math.Abs(c) < 0.1) scaleChange = 0;
+            else scaleChange -= c;
+            master.scale += c;
+        }
     }
 
     protected override void loadControls() {
@@ -54,4 +103,47 @@ public sealed class defaultMode : IMode {
     private defaultMode() {}
     private static readonly Lazy<defaultMode> lazy = new Lazy<defaultMode>(() => new defaultMode());
     public static defaultMode instance => lazy.Value;
+}
+
+internal struct v3Interp {
+    private Vector3 reference, target;
+    private readonly float length;
+    private float startTime;
+    public bool isRunning {get; private set;}
+
+    public v3Interp(float length) {
+        this.length = length;
+
+        reference = Vector3.zero;
+        startTime = 0;
+        target = Vector3.zero;
+        isRunning = false;
+    }
+
+    public void mark(Vector3 reference, Vector3 target) {
+        this.reference = reference;
+        this.target = target;
+        isRunning = true;
+    }
+
+    public void stop() {
+        isRunning = false;
+        startTime = 0;
+    }
+
+    public Vector3 get() {
+        if (!isRunning) return target;
+        if (startTime == 0) startTime = UnityEngine.Time.time;
+
+        float percent = (UnityEngine.Time.time - startTime) / length;
+        if (percent >= 1) {
+            stop();
+            return target;
+        }
+
+        Vector3 d = target - reference;
+        d *= (1f - Mathf.Pow(1f - percent, 4));
+
+        return reference + d;
+    }
 }
