eab328e9930db91777b68750b8a0669b7127c862
Arya Kazemnia <akazemnia24@students.gilman.edu>
2023-08-09 02:05:33 -0400

    started web integration

--- Assets/Code/UI/panels/uiSchedulingPanel.cs
+++ Assets/Code/UI/panels/uiSchedulingPanel.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using System;
+using System.Text;
+using System.Linq;
+using System.IO;
 
 
 public class uiSchedulingPanel : MonoBehaviour
@@ -58,11 +60,36 @@ public class uiSchedulingPanel : MonoBehaviour
 
     public void CreateSat()
     {
-        #if (UNITY_EDITOR || UNITY_STANDALONE) && !UNITY_WEBGL
         representationData rd = new representationData("planet", "defaultMat");
         satellite sat = new satellite(name, new satelliteData(new Timeline(SMAxis, Eccentricity, Inclination, AOP, RAAN, MeanAnom, 1, epochTime, master.planetMu[centerBody.name])), rd, centerBody);
         master.parentBody.Add(sat, centerBody);
         master.relationshipSatellite[centerBody].Add(sat);
-        # endif
+        linkBudgeting.users.Add(sat.name, (false, 0, 1));
+
+        if (web.isClient)
+        {
+            byte[] data;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    bw.Write(sat.name);
+                    bw.Write(SMAxis);
+                    bw.Write(Eccentricity);
+                    bw.Write(Inclination);
+                    bw.Write(AOP);
+                    bw.Write(RAAN);
+                    bw.Write(MeanAnom);
+                    bw.Write(epochTime);
+                    bw.Write(master.planetMu[centerBody.name]);
+                    bw.Write(sat.parent.name);
+
+                }
+
+                data = ms.ToArray();
+            }
+
+            web.sendMessage((byte) userWebHandles.updateSatellites, data);
+        }
     }
 }
